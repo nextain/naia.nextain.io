536ef89d27989d5d463cc79340ec95481354f184
luke.yang <luke.yang@caretive.ai>
2026-02-21 14:22:27 +0900

    chore: update google analytics ID to G-R3HHTFQNGS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
     <html suppressHydrationWarning>
       <head>
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-G2BBWG5BNB"
+          src="https://www.googletagmanager.com/gtag/js?id=G-R3HHTFQNGS"
           strategy="afterInteractive"
         />
         <Script id="gtag-init" strategy="afterInteractive">
@@ -58,7 +58,7 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-G2BBWG5BNB');
+            gtag('config', 'G-R3HHTFQNGS');
           `}
         </Script>
       </head>
